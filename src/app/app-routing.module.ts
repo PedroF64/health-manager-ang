4279df09584f0708ea381d1f965d636f2b4dc6fc
Pedro Fernandes <pedrofernandesprojects@gmail.com>
2023-02-24 19:56:44 +0000

    Better toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: RegisterComponent },
-  { path: 'toolbar', component: ToolbarComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
